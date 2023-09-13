204469c98bf6e4043da5e79a0380b2c16a8a1b18
Mathieu Daix <mathieudaixpro@gmail.com>
2023-09-13 16:56:05 +0200

    test performance number of cubes

--- components/scene/Scene.js
+++ components/scene/Scene.js
@@ -5,7 +5,7 @@ import Pointer from './Pointer'
 import Cube from './Cube'
 import { Perf } from 'r3f-perf'
 
-const cubes = [...Array(50)].map(() => ({ scale: [0.9, 1, 1.1][Math.floor(Math.random() * 3)] }))
+const cubes = [...Array(25)].map(() => ({ scale: [0.9, 1, 1.1][Math.floor(Math.random() * 3)] }))
 
 export default function Scene() {
     return (
